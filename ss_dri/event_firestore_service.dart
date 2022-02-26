4001f81abcb9a4ad31a0046c91fbfcc5afd4e78b
RodrigoVite <78988781+RodrigoVite@users.noreply.github.com>
2022-02-26 15:03:29 -0600

    Delete event_firestore_service.dart

--- ss_dri/event_firestore_service.dart
+++
@@ -1,4 +0,0 @@
-import 'package:firebase_helpers/firebase_helpers.dart';
-import 'event.dart';
-
-final eventDBS = DatabaseService<EventModel>("Eventos/DRI/evento", fromDS: (id,data) => EventModel.fromDS(id, data), toMap:(event) => event.toMap(),);
