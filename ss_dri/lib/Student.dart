3b35fe61ff32c9826e02219d608aa6d53e98f707
DavidEstrada99 <78774860+DavidEstrada99@users.noreply.github.com>
2022-03-25 21:05:48 -0600

    Notificación global activada

--- ss_dri/lib/Student.dart
+++ ss_dri/lib/Student.dart
@@ -7,7 +7,7 @@ import 'model/view_event_empty.dart';
 import 'package:intl/intl.dart';
 import 'model/view_event.dart';
 import 'model/event.dart';
-
+import 'model/notification_service.dart';
 
 final Map<DateTime, List> _holidays = {
   DateTime(2022, 1, 1): ['New Year\'s Day'],
@@ -67,13 +67,22 @@ class _StudentState extends State<StudentHome> with TickerProviderStateMixin {
     print('CALLBACK: _onCalendarCreated');
   }
 
-  /*Agrupa eventos por fechas. Detecta cambios*/ 
+  /*Agrupa eventos por fechas. Detecta cambios*/
   Map<DateTime, List<dynamic>> _groupEvents(List<EventModel> events) {
     Map<DateTime, List<dynamic>> data = {};
-
-    events.forEach((event) { 
-      DateTime date = DateTime(event.eventDate.year, event.eventDate.month, event.eventDate.day, 12);
-      if(data[date] == null) data[date] = [];
+    events.forEach((event) {
+      if (event.suscription) {
+        int id = 0;
+        for (int i = 0; i < event.id.length; i++) {
+          id = id + event.id.codeUnitAt(i);
+        }
+        print("ID: $id");
+        NotificationService().CreatingNotifications(
+            id, event.title, event.description, event.eventDate);
+      }
+      DateTime date = DateTime(
+          event.eventDate.year, event.eventDate.month, event.eventDate.day, 12);
+      if (data[date] == null) data[date] = [];
       data[date].add(event);
     });
     return data;
@@ -87,84 +96,94 @@ class _StudentState extends State<StudentHome> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 132, 43, 87),
-        centerTitle: true,
-        title: Text('Bienvenido'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 132, 43, 87),
-              ),
-              child: Text(
-                'D.R.I',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 80,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Profile'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Cerrar sesión'),
-              onTap: () {
-                _signOut();
-              },
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 132, 43, 87),
+          centerTitle: true,
+          title: Text('Bienvenido'),
         ),
-      ),
-      body: StreamBuilder<List<EventModel>>(
-        stream: eventDBS.streamList(),
-        builder: (context, snapshot) {
-          if(snapshot.hasData){
-            List<EventModel> allEvents = snapshot.data;
-            if(allEvents.isNotEmpty){
-              _events = _groupEvents(allEvents);
-            }
-          }
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _buildTableCalendar(),
-                const SizedBox(
-                  height: 8.0,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 132, 43, 87),
                 ),
-                const SizedBox(
-                  height: 8.0,
+                child: Text(
+                  'D.R.I',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 80,
+                  ),
                 ),
-                ..._selectedEvents.map((event) => ListTile(
-                  title: Text(event.title),
-                  subtitle: Text(DateFormat("EEEE, dd MMMM, yyyy",'es_ES').format(event.eventDate)),
-                  onTap: () {
-                    if(event.suscription){  //evento con notificación obligatoria.
-                      Navigator.push(context, MaterialPageRoute( 
-                        builder: (_) => EventDetailsPageEmpty( event: event,)));
-                    }else{                  //evento con notificación activable.
-                      Navigator.push(context, MaterialPageRoute( 
-                        builder: (_) => EventDetailsPage( event: event,)));
-                    }
-                  },
-                )),
-              ],
-            ),
-          );
-        }
-      )
-    );
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('Cerrar sesión'),
+                onTap: () {
+                  _signOut();
+                },
+              ),
+            ],
+          ),
+        ),
+        body: StreamBuilder<List<EventModel>>(
+            stream: eventDBS.streamList(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<EventModel> allEvents = snapshot.data;
+                if (allEvents.isNotEmpty) {
+                  _events = _groupEvents(allEvents);
+                }
+              }
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _buildTableCalendar(),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    ..._selectedEvents.map((event) => ListTile(
+                          title: Text(event.title),
+                          subtitle: Text(
+                              DateFormat("EEEE, dd MMMM, yyyy", 'es_ES')
+                                  .format(event.eventDate)),
+                          onTap: () {
+                            if (event.suscription) {
+                              //evento con notificación obligatoria.
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => EventDetailsPageEmpty(
+                                            event: event,
+                                          )));
+                            } else {
+                              //evento con notificación activable.
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => EventDetailsPage(
+                                            event: event,
+                                          )));
+                            }
+                          },
+                        )),
+                  ],
+                ),
+              );
+            }));
   }
 
   Widget _buildTableCalendar() {
